575ac9f75f9e0e603bab220446d02eb78a7e9f57
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-18 10:06:27 +0100

    feat: (page.tsx): Add jsx and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>main</main>
+    <main className='h-full relative'>
+      <div className='fixed bottom-4 left-0 right-0'>
+        <form className='p-4 bg-base-200 max-w-lg w-full'>
+          <fieldset className='flex gap-4 items-start'>
+            <textarea
+              className='textarea textarea-primary'
+              placeholder='Message'
+            />
+            <button className='btn btn-primary' />
+          </fieldset>
+        </form>
+      </div>
+    </main>
   );
 }
