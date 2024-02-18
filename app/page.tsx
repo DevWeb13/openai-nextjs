3bc59e2f80cd5383fdf3ec9e72787631d1bf0a12
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-18 10:37:23 +0100

    Add and test route POST api for form

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,38 @@ import { Sparkles } from 'lucide-react';
 import Image from 'next/image';
 
 export default function Home() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const prompt = formData.get('prompt') as string;
+
+    const result = await fetch('/api/tailwind', {
+      method: 'POST',
+      body: JSON.stringify({ prompt }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const json = await result.json();
+
+    console.log(json);
+  };
+
   return (
     <main className='h-full relative'>
       <div className='absolute top-4 left-0 right-0 flex items-center justify-center'>
         <progress className='progress w-56' />
       </div>
       <div className='fixed bottom-4 left-0 right-0 flex items-center justify-center'>
-        <form className='p-4 bg-base-200 max-w-lg w-full'>
+        <form
+          className='p-4 bg-base-200 max-w-lg w-full'
+          onSubmit={handleSubmit}>
           <fieldset className='flex gap-4 items-start'>
             <textarea
+              name='prompt'
               className='w-full textarea textarea-primary'
               placeholder='Message'
             />
