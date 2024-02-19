126312533145fcf7e3f892623fe15816cf1ccdc4
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-19 17:22:55 +0100

    Connect and display stream response of OpenAPI

--- app/api/tailwind/route.ts
+++ app/api/tailwind/route.ts
@@ -1,5 +1,25 @@
-import { NextResponse } from "next/server"
+import { openai } from '@/src/lib/openai';
+import { NextResponse } from 'next/server';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 export const POST = async (req: Request) => {
-  return NextResponse.json({ message: "Hello, World!" })
-}
+  const { prompt } = await req.json();
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4',
+    stream: true,
+    messages: [
+      {
+        role: 'assistant',
+        content: 'Write only Tailwind code',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,65 @@
 
 import { Sparkles } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const [htmlCode, setHtmlCode] = useState('');
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (loading) return;
 
     const formData = new FormData(event.currentTarget);
 
     const prompt = formData.get('prompt') as string;
 
+    setLoading(true);
+    setHtmlCode('');
+
     const result = await fetch('/api/tailwind', {
       method: 'POST',
       body: JSON.stringify({ prompt }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
     });
 
-    const json = await result.json();
+    const body = result.body as ReadableStream;
 
-    console.log(json);
+    if (!body) {
+      alert('No response from server');
+      setLoading(false);
+      return;
+    }
+
+    const reader = body.getReader();
+
+    const readChunk = async () => {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        // terminé
+        return;
+      }
+
+      const chunk = new TextDecoder().decode(value);
+      setHtmlCode((prevHtmlCode) => prevHtmlCode + chunk);
+      await readChunk();
+    };
+
+    await readChunk();
   };
 
   return (
     <main className='h-full relative'>
-      <div className='absolute top-4 left-0 right-0 flex items-center justify-center'>
-        <progress className='progress w-56' />
-      </div>
+      {loading && (
+        <div className='absolute top-4 left-0 right-0 flex items-center justify-center'>
+          <progress className='progress w-56' />
+        </div>
+      )}
+
+      <pre>{htmlCode}</pre>
+
       <div className='fixed bottom-4 left-0 right-0 flex items-center justify-center'>
         <form
           className='p-4 bg-base-200 max-w-lg w-full'
